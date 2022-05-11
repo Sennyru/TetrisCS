b818733e49d06768e852ea97df05b624affb3f59
Secon0101 <duwnsgur11012@gmail.com>
2022-05-11 19:47:17 +0900

    더미 코드 삭제

--- TetrisCS/BlockType.cs
+++ TetrisCS/BlockType.cs
@@ -2,9 +2,4 @@
 {
     /// <summary> 블록 모양의 종류 </summary>
     public enum BlockType { I, J, L, O, S, T, Z }
-
-    static class BlockTypeExtensions
-    {
-        public static int a(this BlockType b) => 1;
-    }
 }
